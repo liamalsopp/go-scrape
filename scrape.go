15e4341103a6e989e4cee392563e4fa60818cb4a
Liam Alsopp <liam.alsopp@otterbyte.co.uk>
2025-11-29 16:40:51 +0000

    copied from example chromedp

--- scrape.go
+++ scrape.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/chromedp"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// create context
+	ctx, cancel := chromedp.NewContext(
+		context.Background(),
+		chromedp.WithLogf(log.Printf),
+	)
+	defer cancel()
+
+	// create a timeout as a safety net to prevent any infinite wait loops
+	ctx, cancel = context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+
+	// set up a channel, so we can block later while we monitor the download
+	// progress
+	done := make(chan bool)
+
+	// set the download url as the chromedp GitHub user avatar
+	urlstr := "https://avatars.githubusercontent.com/u/33149672"
+
+	// this will be used to capture the request id for matching network events
+	var requestID network.RequestID
+
+	// set up a listener to watch the network events and close the channel when
+	// complete the request id matching is important both to filter out
+	// unwanted network events and to reference the downloaded file later
+	chromedp.ListenTarget(ctx, func(v interface{}) {
+		switch ev := v.(type) {
+		case *network.EventRequestWillBeSent:
+			log.Printf("EventRequestWillBeSent: %v: %v", ev.RequestID, ev.Request.URL)
+			if ev.Request.URL == urlstr {
+				requestID = ev.RequestID
+			}
+		case *network.EventLoadingFinished:
+			log.Printf("EventLoadingFinished: %v", ev.RequestID)
+			if ev.RequestID == requestID {
+				close(done)
+			}
+		}
+	})
+
+	// all we need to do here is navigate to the download url
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate(urlstr),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	// This will block until the chromedp listener closes the channel
+	<-done
+	// get the downloaded bytes for the request id
+	var buf []byte
+	if err := chromedp.Run(ctx, chromedp.ActionFunc(func(ctx context.Context) error {
+		var err error
+		buf, err = network.GetResponseBody(requestID).Do(ctx)
+		return err
+	})); err != nil {
+		log.Fatal(err)
+	}
+
+	// write the file to disk - since we hold the bytes we dictate the name and
+	// location
+	if err := os.WriteFile("download.png", buf, 0644); err != nil {
+		log.Fatal(err)
+	}
+	log.Print("wrote download.png")
 }
